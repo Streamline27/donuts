468d73f58d64c341ad244ad2459b1a5eb13335a5
Vladislav Yakushin <vladislavs.jakusins@citadele.lv>
2019-05-06 16:01:48 +0300

    Added codegen and rabbit

---
+++ rabbit/src/main/java/lv/citadele/rabbit/RabbitApplication.java
@@ -0,0 +1,19 @@
+package lv.citadele.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class RabbitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Rabbit";
+    }
+}
