b7033d9f3e1349faa2d9532c5bb6c33b059765d2
Vladislav Yakushin <vladislavs.jakusins@citadele.lv>
2019-05-06 19:32:59 +0300

    Producer created

--- rabbit/src/main/java/lv/citadele/rabbit/messages/MessageController.java
+++ rabbit/src/main/java/lv/citadele/rabbit/messages/MessageController.java
@@ -1,16 +1,27 @@
 package lv.citadele.rabbit.messages;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 public class MessageController {
     private final MessageDao dao;
+    private final RabbitTemplate rabbitTemplate;
 
     @Autowired
-    public MessageController(MessageDao dao) {
+    public MessageController(MessageDao dao, RabbitTemplate rabbitTemplate) {
         this.dao = dao;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @GetMapping("/message/create/{text}")
+    public String create(@PathParam("text") String text) {
+        rabbitTemplate.convertAndSend(text);
+        return "Great success";
     }
 
     @GetMapping("/messages")
